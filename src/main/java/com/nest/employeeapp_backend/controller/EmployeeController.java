0fed234db22eaa056239219c9e92a71fde6c7842
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-05 15:38:54 +0530

    home

--- src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
@@ -1,9 +1,15 @@
 package com.nest.employeeapp_backend.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-
+    @GetMapping("/")
+    public String HomePage()
+    {
+        return "welcome to employee home page";
+    }
 }
