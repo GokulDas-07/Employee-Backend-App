ce300981c2c95938caf6e99aa50ead03c01151fb
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-06 15:42:17 +0530

    post

--- src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
@@ -1,46 +1,56 @@
 package com.nest.employeeapp_backend.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.nest.employeeapp_backend.dao.EmployeeDao;
+import com.nest.employeeapp_backend.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
+    @Autowired
+    private EmployeeDao dao;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
     public String HomePage()
     {
         return "welcome to employee home page";
     }
 
-    @GetMapping("/add")
-    public String AddEmployee()
+    @PostMapping(path= "/add",consumes = "application/json",produces = "application/jsom")
+    public String AddEmployee(@RequestBody Employee e)
     {
-        return "Add Employee Page";
+        System.out.println(e.toString());
+        System.out.println("Employee id:"+e.getId());
+        dao.save(e);
+        return "Added Employee";
     }
 
-    @GetMapping("/edit")
+    /*@PostMapping("/edit")
     public String EditEmployee()
     {
         return "Edit employee page";
     }
 
-    @GetMapping("/search")
+    @PostMapping("/search")
     public String SearchEmployee()
     {
         return "Search Employee page";
-    }
-
+    }*/
+    @CrossOrigin(origins = "*")
     @GetMapping("/view")
-    public String ViewEmployees()
+    public List<Employee> ViewEmployees()
     {
-        return "View Employees page";
+        return (List<Employee>) dao.findAll();
     }
 
-    @GetMapping("/delete")
+    /*@PostMapping("/delete")
     public String DeleteEmployee()
     {
         return "Delete Employees page";
-    }
+    }*/
 
 }
